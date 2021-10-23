ea917ee65f08c7581f808dedba973c84c50e157e
Mark Hagemann <mhagemann09@gmail.com>
2021-10-24 02:52:05 +1100

    Update nvim-tree and remove project.nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -113,17 +113,6 @@ local function plugins(use)
 		end,
 	})
 
-	use({
-		"ahmedkhalf/project.nvim",
-		config = function()
-			require("project_nvim").setup({
-				-- your configuration comes here
-				-- or leave it empty to use the default settings
-				-- refer to the configuration section below
-			})
-		end,
-	})
-
 	-- Theme: color schemes
 	use({
 		"folke/tokyonight.nvim",
