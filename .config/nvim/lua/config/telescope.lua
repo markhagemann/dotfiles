41861b38ba0b21f5b3df27afddf496e9b3a2e2a7
Mark Hagemann <mhagemann@trevipay.com>
2021-11-03 20:01:31 +1100

    Apply telescope styling updates

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -14,8 +14,28 @@ telescope.setup({
     },
   },
   defaults = {
+    layout_config = { prompt_position = "top", height = 0.4, vertical = { width = 0.5 } },
+    layout_strategy = "vertical",
+    sorting_strategy = "ascending",
+    pickers = {
+      buffers = {
+        show_all_buffers = true,
+        sort_lastused = true,
+      },
+      find_files = {
+        hidden = true,
+      },
+      live_grep = {
+        layout_config = { prompt_position = "bottom", height = 0.6 },
+        layout_strategy = "horizontal",
+        preview = { check_mime_type = false },
+      },
+    },
+    -- preview = {
+    --   check_mime_type = false,
+    -- },
+    preview = false,
     mappings = { i = { ["<c-t>"] = trouble.open_with_trouble } },
-    -- mappings = { i = { ["<esc>"] = actions.close } },
     vimgrep_arguments = {
       "rg",
       "--color=never",
@@ -25,46 +45,10 @@ telescope.setup({
       "--column",
       "--smart-case",
     },
-    -- prompt_position = "bottom",
     prompt_prefix = " ",
     selection_caret = " ",
-    -- entry_prefix = "  ",
-    -- initial_mode = "insert",
-    -- selection_strategy = "reset",
-    -- sorting_strategy = "descending",
-    -- layout_strategy = "horizontal",
-    -- layout_defaults = {
-    --   horizontal = {
-    --     mirror = false,
-    --   },
-    --   vertical = {
-    --     mirror = false,
-    --   },
-    -- },
-    -- file_sorter = require"telescope.sorters".get_fzy_file
     file_ignore_patterns = { "node_modules", ".lock" },
-    -- generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
-    -- shorten_path = true,
     winblend = 10,
-    -- width = 0.7,
-    -- preview_cutoff = 120,
-    -- results_height = 1,
-    -- results_width = 0.8,
-    -- border = {},
-    -- borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
-    -- borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
-    -- prompt = { "─", "│", " ", "│", "┌", "┐", "│", "│" },
-    -- results = { "─", "│", "─", "│", "├", "┤", "┘", "└" },
-    -- preview = { "─", "│", "─", "│", "┌", "┐", "┘", "└" }
-    -- color_devicons = true,
-    -- use_less = true,
-    -- set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
-    -- file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
-    -- grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
-    -- qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
-
-    -- -- Developer configurations: Not meant for general override
-    -- buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
   },
 })
 
@@ -74,10 +58,12 @@ telescope.load_extension("fzf")
 local util = require("util")
 
 util.nnoremap("<C-p>", function()
-  require("telescope.builtin").find_files()
-end)
-util.nnoremap("<Leader>fd", function()
-  require("telescope.builtin").find_files({ cwd = "~/.config/" })
+  require("telescope.builtin").find_files({
+    hidden = true,
+    layout_config = { prompt_position = "top", height = 0.3 },
+    layout_strategy = "vertical",
+    sorting_strategy = "ascending",
+  })
 end)
 
 util.nnoremap("<leader>fz", function()
