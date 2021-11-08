605c96b10960d180d4b81d30cecec4f145f2fcc8
Mark Hagemann <mhagemann09@gmail.com>
2021-11-09 00:34:23 +1100

    Switch to prettierd from prettier_d_slim

--- .config/nvim/lua/config/lsp/null-ls.lua
+++ .config/nvim/lua/config/lsp/null-ls.lua
@@ -6,7 +6,7 @@ function M.setup(options)
     debounce = 150,
     save_after_format = false,
     sources = {
-      nls.builtins.formatting.prettier_d_slim,
+      nls.builtins.formatting.prettierd,
       nls.builtins.formatting.stylua,
       -- nls.builtins.formatting.fish_indent,
       nls.builtins.formatting.fixjson.with({ filetypes = { "jsonc" } }),
