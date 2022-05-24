135ddba06911601ef73ec548dbec401a07ea605a
Mark Hagemann <mhagemann@trevipay.com>
2022-05-24 12:40:23 +1000

    Replace blamer with blame functionality in gitsigns

--- .config/nvim/lua/config/gitsigns.lua
+++ .config/nvim/lua/config/gitsigns.lua
@@ -1,4 +1,12 @@
 require("gitsigns").setup({
+  current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+    delay = 1000,
+    ignore_whitespace = false,
+  },
+  current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> | <summary>",
   signs = {
     add = { hl = "GitSignsAdd", text = "▍", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
     change = {
