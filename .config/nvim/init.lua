3d33a8c170bf066a7f4a2c0497b05ef8cdec9006
Mark Hagemann <mhagemann@trevipay.com>
2022-06-06 13:33:33 +1000

    Initial changes for nvim-from-scratch

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,25 @@
-require("util")
-require("options")
-
--- no need to load this immediately, since we have packer_compiled
-vim.defer_fn(function()
-  require("plugins")
-end, 0)
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
+require "user.colorscheme"
+require "user.cmp"
+require "user.lsp"
+require "user.telescope"
+require "user.treesitter"
+require "user.autopairs"
+require "user.comment"
+require "user.gitsigns"
+require "user.nvim-tree"
+require "user.bufferline"
+require "user.lualine"
+require "user.toggleterm"
+require "user.project"
+require "user.impatient"
+require "user.indentline"
+require "user.alpha"
+require "user.whichkey"
+require "user.colorizer"
+require "user.neoscroll"
+require "user.trouble"
+require "user.specs"
+require "user.autocommands"
