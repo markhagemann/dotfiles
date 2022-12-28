acf63bbfc03be6a1aa668f5db7516848ab9d81ae
Mark Hagemann <mhagemann@trevipay.com>
2022-12-28 12:10:14 +1100

    Update tokyonight settings

--- .config/nvim/lua/plugins/configs/tokyonight.lua
+++ .config/nvim/lua/plugins/configs/tokyonight.lua
@@ -3,52 +3,62 @@ if not status_ok then
   return
 end
 
-vim.g.material_style = "palenight"
-vim.g.material_italic_comments = 1
-vim.g.material_italic_keywords = 1
-vim.g.material_italic_functions = 1
-vim.g.material_lsp_underline = 1
-
-vim.g.sonokai_style = "atlantis"
-vim.g.sonokai_enable_italic = 1
-vim.g.sonokai_disable_italic_comment = 1
-vim.g.sonokai_diagnostic_virtual_text = "colored"
-
-vim.g.edge_style = "neon"
-vim.g.edge_enable_italic = 1
-vim.g.edge_disable_italic_comment = 0
-vim.g.edge_transparent_background = 0
-
-vim.g.embark_terminal_italics = 1
-
-vim.g.nightflyTransparent = 0
-
-vim.g.nvcode_termcolors = 256
-
-vim.o.background = "dark"
-
-vim.g.tokyonight_dev = true
-vim.g.tokyonight_style = "storm"
-vim.g.tokyonight_sidebars = {
-  "qf",
-  "vista_kind",
-  "terminal",
-  "packer",
-  "spectre_panel",
-  "NeogitStatus",
-  "help",
-}
-vim.g.tokyonight_cterm_colors = false
-vim.g.tokyonight_terminal_colors = true
-vim.g.tokyonight_italic_comments = true
-vim.g.tokyonight_italic_keywords = true
-vim.g.tokyonight_italic_functions = false
-vim.g.tokyonight_italic_variables = false
-vim.g.tokyonight_transparent = false
-vim.g.tokyonight_hide_inactive_statusline = true
-vim.g.tokyonight_dark_sidebar = true
-vim.g.tokyonight_dark_float = true
-vim.g.tokyonight_colors = {}
+-- vim.g.tokyonight_dev = true
+-- vim.g.tokyonight_style = "storm"
+-- vim.g.tokyonight_sidebars = {
+--   "qf",
+--   "vista_kind",
+--   "terminal",
+--   "packer",
+--   "spectre_panel",
+--   "NeogitStatus",
+--   "help",
+-- }
+-- vim.g.tokyonight_cterm_colors = false
+-- vim.g.tokyonight_terminal_colors = true
+-- vim.g.tokyonight_italic_comments = true
+-- vim.g.tokyonight_italic_keywords = true
+-- vim.g.tokyonight_italic_functions = false
+-- vim.g.tokyonight_italic_variables = false
+-- vim.g.tokyonight_transparent = true
+-- vim.g.tokyonight_hide_inactive_statusline = true
+-- vim.g.tokyonight_dark_sidebar = true
+-- vim.g.tokyonight_dark_float = true
+-- vim.g.tokyonight_colors = {}
 -- vim.g.tokyonight_colors = { border = "orange" }
 
-tokyonight.colorscheme()
+tokyonight.colorscheme({
+  -- your configuration comes here
+  -- or leave it empty to use the default settings
+  style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+  light_style = "day", -- The theme is used when the background is set to light
+  transparent = true, -- Enable this to disable setting the background color
+  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+  styles = {
+    -- Style to be applied to different syntax groups
+    -- Value is any valid attr-list value for `:help nvim_set_hl`
+    comments = { italic = true },
+    keywords = { italic = true },
+    functions = {},
+    variables = {},
+    -- Background styles. Can be "dark", "transparent" or "normal"
+    sidebars = "dark", -- style for sidebars, see below
+    floats = "dark", -- style for floating windows
+  },
+  sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+  day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+  hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+  dim_inactive = true, -- dims inactive windows
+  lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
+
+  --- You can override specific color groups to use other groups or a hex color
+  --- function will be called with a ColorScheme table
+  ---@param colors ColorScheme
+  on_colors = function(colors) end,
+
+  --- You can override specific highlights to use other groups or a hex color
+  --- function will be called with a Highlights and ColorScheme table
+  ---@param highlights Highlights
+  ---@param colors ColorScheme
+  on_highlights = function(highlights, colors) end,
+})
