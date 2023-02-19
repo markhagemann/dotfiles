67fd9aed8d5ece3ea66c2a7c4ea9280c3170d55c
Mark Hagemann <mhagemann@trevipay.com>
2023-02-20 10:50:24 +1100

    Update opts

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,6 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+opt.conceallevel = 0 -- Don't conceal anything
+
 return {
   opts = { style = "tokyonight-storm" },
 }
