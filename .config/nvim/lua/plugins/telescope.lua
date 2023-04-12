6035052ec4190160f54cc53cb1a49f48bccd88a3
Mark Hagemann <mhagemann@trevipay.com>
2023-04-12 10:49:46 +1000

    Add telescope-fzf

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -14,4 +14,15 @@ return {
       },
     },
   },
+  -- add telescope-fzf-native
+  {
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+  },
 }
