6c6be9256a9724cf221211396aeedab3cbfa4af6
Mark Hagemann <mhagemann@trevipay.com>
2023-10-13 11:38:11 +1100

    Update tmux and kitty config. Remove unnecessary autocommands and mason / none-ls configuration

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -2,10 +2,10 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
-  command = "if mode() != 'c' | checktime | endif",
-  pattern = { "*" },
-})
+-- vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+--   command = "if mode() != 'c' | checktime | endif",
+--   pattern = { "*" },
+-- })
 
 -- Disable autoformat for lua and json files
 -- vim.api.nvim_create_autocmd({ "FileType" }, {
