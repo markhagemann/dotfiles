20a6d20f7b36d9dc898cbb391e6906c4d018e93c
Mark Hagemann <mhagemann@trevipay.com>
2023-12-18 12:23:04 +1100

    Update tmux display settings and add nvim-lsp-file-operations to nvim config

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,5 @@
 return {
+  { "ahmedkhalf/project.nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -110,4 +110,14 @@ return {
       })
     end,
   },
+  {
+    "antosha417/nvim-lsp-file-operations",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-tree.lua",
+    },
+    config = function()
+      require("lsp-file-operations").setup()
+    end,
+  },
 }
