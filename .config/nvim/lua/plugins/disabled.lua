d55bfc26205101573e4c58e7a2a3ceb6cfee5666
Mark Hagemann <mhagemann@trevipay.com>
2023-12-31 00:36:26 +1100

    Add nvchad configuration

--- .config/nvim/lua/plugins/disabled.lua
+++
@@ -1,7 +0,0 @@
-return {
-  { "ahmedkhalf/project.nvim", enabled = false },
-  { "akinsho/bufferline.nvim", enabled = false },
-  { "folke/flash.nvim", enabled = false },
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-  { "SmiteshP/nvim-navic", enabled = false },
-}
--- .config/nvim/lua/plugins/syntax.lua
+++
@@ -1,4 +0,0 @@
-return {
-  { "joukevandermaas/vim-ember-hbs" },
-  { "m-demare/hlargs.nvim" },
-}
