0fbb1e791a4321255ba70a46d099933ace165af4
Mark Hagemann <mhagemann@trevipay.com>
2024-01-01 00:34:29 +1100

    Update colorscheme settings

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -54,8 +54,3 @@ keymap.set("n", "<leader>to", ":tabnew<CR>")   -- open new tab
 keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>tn", ":tabn<CR>")     --  go to next tab
 keymap.set("n", "<leader>tp", ":tabp<CR>")     --  go to previous tab
-
-----------------------
--- Plugin Keybinds
-----------------------
--- telescope
--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -27,46 +27,6 @@ return {
       for hl, col in pairs(TelescopeColor) do
         vim.api.nvim_set_hl(0, hl, col)
       end
-
-      require("catppuccin").setup({
-        flavour = "mocha",             -- latte, frappe, macchiato, mocha
-        transparent_background = true, -- disables setting the background color.
-        -- dim_inactive = {
-        --   enabled = false, -- dims the background color of inactive window
-        --   shade = "dark",
-        --   percentage = 0.15, -- percentage of the shade to apply to the inactive window
-        -- },
-        -- no_italic = false, -- Force no italic
-        -- no_bold = false, -- Force no bold
-        -- no_underline = false, -- Force no underline
-        styles = {                 -- Handles the styles of general hi groups (see `:h highlight-args`):
-          comments = { "italic" }, -- Change the style of comments
-          conditionals = { "italic" },
-          -- loops = {},
-          -- functions = {},
-          -- keywords = {},
-          -- strings = {},
-          -- variables = {},
-          -- numbers = {},
-          -- booleans = {},
-          -- properties = {},
-          -- types = {},
-          -- operators = {},
-        },
-        integrations = {
-          telescope = true,
-          -- cmp = true,
-          -- gitsigns = true,
-          -- nvimtree = true,
-          -- notify = false,
-          -- treesitter = true,
-          -- mini = {
-          --   enabled = true,
-          --   indentscope_color = "",
-          -- },
-          -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-        },
-      })
     end,
   },
   {
