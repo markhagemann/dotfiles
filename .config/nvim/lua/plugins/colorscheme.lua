f9c61f312bcbc66c8c4e7cb96fef47b8743381d5
Mark Hagemann <mhagemann@trevipay.com>
2024-01-01 00:25:11 +1100

    Update colorscheme settings and fix redundant use of plugin mappings in keymaps

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -6,6 +6,67 @@ return {
     priority = 1000,
     config = function()
       vim.cmd.colorscheme("catppuccin")
+
+      local colors = require("catppuccin.palettes").get_palette()
+      local TelescopeColor = {
+        TelescopeMatching = { fg = colors.flamingo },
+        TelescopeSelection = { fg = colors.text, bg = colors.surface0, bold = true },
+
+        TelescopePromptPrefix = { bg = colors.surface0 },
+        TelescopePromptNormal = { bg = colors.surface0 },
+        TelescopeResultsNormal = { bg = colors.mantle },
+        TelescopePreviewNormal = { bg = colors.mantle },
+        TelescopePromptBorder = { bg = colors.surface0, fg = colors.surface0 },
+        TelescopeResultsBorder = { bg = colors.mantle, fg = colors.mantle },
+        TelescopePreviewBorder = { bg = colors.mantle, fg = colors.mantle },
+        TelescopePromptTitle = { bg = colors.pink, fg = colors.mantle },
+        TelescopeResultsTitle = { fg = colors.mantle },
+        TelescopePreviewTitle = { bg = colors.green, fg = colors.mantle },
+      }
+
+      for hl, col in pairs(TelescopeColor) do
+        vim.api.nvim_set_hl(0, hl, col)
+      end
+
+      require("catppuccin").setup({
+        flavour = "mocha",             -- latte, frappe, macchiato, mocha
+        transparent_background = true, -- disables setting the background color.
+        -- dim_inactive = {
+        --   enabled = false, -- dims the background color of inactive window
+        --   shade = "dark",
+        --   percentage = 0.15, -- percentage of the shade to apply to the inactive window
+        -- },
+        -- no_italic = false, -- Force no italic
+        -- no_bold = false, -- Force no bold
+        -- no_underline = false, -- Force no underline
+        styles = {                 -- Handles the styles of general hi groups (see `:h highlight-args`):
+          comments = { "italic" }, -- Change the style of comments
+          conditionals = { "italic" },
+          -- loops = {},
+          -- functions = {},
+          -- keywords = {},
+          -- strings = {},
+          -- variables = {},
+          -- numbers = {},
+          -- booleans = {},
+          -- properties = {},
+          -- types = {},
+          -- operators = {},
+        },
+        integrations = {
+          telescope = true,
+          -- cmp = true,
+          -- gitsigns = true,
+          -- nvimtree = true,
+          -- notify = false,
+          -- treesitter = true,
+          -- mini = {
+          --   enabled = true,
+          --   indentscope_color = "",
+          -- },
+          -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+        },
+      })
     end,
   },
   {
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,3 +1,16 @@
+local builtin = require("telescope.builtin")
+local keymap = vim.keymap
+
+keymap.set(
+  "n",
+  "<C-p>",
+  "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>"
+)                                                     -- find files within current working directory
+keymap.set("n", "<leader>/", builtin.live_grep, {})   -- find string in current working directory as you type
+keymap.set("n", "<leader>.", builtin.grep_string, {}) -- find string under cursor in current working directory
+keymap.set("n", "<leader>,", builtin.buffers, {})     -- list open buffers in current neovim instance
+keymap.set("n", "<leader>ht", builtin.help_tags, {})  -- list available help tags
+
 return {
   {
     "nvim-telescope/telescope.nvim",
@@ -16,16 +29,13 @@ return {
             require("telescope.themes").get_dropdown({}),
           },
           fzf = {
-            fuzzy = true, -- false will only do exact matching
+            fuzzy = true,                   -- false will only do exact matching
             override_generic_sorter = true, -- override the generic sorter
-            override_file_sorter = true, -- override the file sorter
-            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+            override_file_sorter = true,    -- override the file sorter
+            case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
           },
         },
       })
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 
       require("telescope").load_extension("ui-select")
     end,
