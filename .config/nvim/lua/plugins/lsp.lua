e15256bc9d6b2f1381f6117c182eb83137cf13ca
Mark Hagemann <mhagemann@trevipay.com>
2024-01-04 16:42:32 +1100

    Update statusline and nvim-ufo to not display folds on outline

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -275,16 +275,6 @@ return {
       require("lspsaga").setup({
         debug = false,
         use_saga_diagnostic_sign = true,
-        ui = {
-          -- disable lightbulb at end of line
-          code_action = "",
-          border = "rounded",
-          winblend = 0,
-          expand = "",
-          collapse = "",
-          diagnostic = "",
-          kind = {},
-        },
         -- diagnostic sign
         error_sign = "",
         warn_sign = "",
@@ -298,6 +288,68 @@ return {
         finder_definition_icon = "  ",
         finder_reference_icon = "  ",
         max_preview_lines = 10,
+
+        request_timeout = 2000,
+
+        ui = {
+          -- currently only round theme
+          theme = "round",
+          -- this option only work in neovim 0.9
+          title = false,
+          -- border type can be single,double,rounded,solid,shadow.
+          border = "rounded",
+          winblend = 0,
+          expand = "",
+          collapse = "",
+          preview = " ",
+          code_action = "",
+          diagnostic = "",
+          incoming = " ",
+          outgoing = " ",
+          colors = {
+            --float window normal background color
+            normal_bg = "#141423",
+            --title background color
+            title_bg = "#FF7070",
+            red = "#e95678",
+            magenta = "#b33076",
+            orange = "#FF8700",
+            yellow = "#f7bb3b",
+            green = "#afd700",
+            cyan = "#36d0e0",
+            blue = "#61afef",
+            purple = "#CBA6F7",
+            white = "#d1d4cf",
+            black = "#1c1c19",
+          },
+          kind = {},
+        },
+
+        outline = {
+          win_position = "right",
+          win_with = "",
+          win_width = 30,
+          show_detail = true,
+          auto_preview = true,
+          auto_refresh = true,
+          auto_close = true,
+          custom_sort = nil,
+          keys = {
+            jump = "o",
+            expand_collapse = "u",
+            quit = "q",
+          },
+        },
+
+        symbol_in_winbar = {
+          enable = true,
+          separator = " ",
+          hide_keyword = true,
+          show_file = false,
+          folder_level = 2,
+          respect_root = false,
+          color_mode = true,
+        },
 
         finder_action_keys = {
           open = "o",
@@ -348,7 +400,7 @@ return {
   },
   {
     "simrat39/symbols-outline.nvim",
-    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    keys = { { "<leader>so", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
     config = function()
       require("symbols-outline").setup()
     end,
