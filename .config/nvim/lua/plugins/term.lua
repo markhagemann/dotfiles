b40386e7c99f13db134dc96e2aef5eeb3371e588
Mark Hagemann <mhagemann@trevipay.com>
2024-01-10 18:12:13 +1100

    Use toggleterm instaed of fterm

--- .config/nvim/lua/plugins/term.lua
+++ .config/nvim/lua/plugins/term.lua
@@ -1,31 +1,44 @@
 return {
   {
-    "numToStr/FTerm.nvim",
+    "akinsho/toggleterm.nvim",
     event = "VeryLazy",
+    version = "*",
     config = function()
-      local fterm = require("FTerm")
-      vim.api.nvim_set_keymap("n", "<leader>tt", ":lua require('FTerm').toggle()<CR>", { noremap = true })
-      vim.api.nvim_set_keymap("t", "<leader>tt", '<C-\\><C-n>:lua require("FTerm").toggle()<CR>', { noremap = true })
-
-      fterm.setup({
-        blend = 5,
-        dimensions = {
-          height = 0.95,
-          width = 0.95,
-          x = 0.5,
-          y = 0.5,
-        },
-      })
-
-      local lazygit = fterm:new({
-        ft = "fterm_lazygit", -- You can also override the default filetype, if you want
+      local Terminal = require("toggleterm.terminal").Terminal
+      local lazygit = Terminal:new({
         cmd = "lazygit",
+        dir = "git_dir",
+        direction = "float",
+        float_opts = {
+          border = "double",
+        },
+        -- function to run on opening the terminal
+        on_open = function(term)
+          vim.cmd("startinsert!")
+          vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+        end,
+        -- function to run on closing the terminal
+        on_close = function(term)
+          vim.cmd("startinsert!")
+        end,
       })
 
-      -- Use this to toggle gitui in a floating terminal
-      vim.keymap.set("n", "<leader>lg", function()
+      function _lazygit_toggle()
         lazygit:toggle()
-      end)
+      end
+
+      function _G.set_terminal_keymaps()
+        local opts = { buffer = 0 }
+        vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+        vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+        vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+        vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+        vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+        vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+        vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+      end
+
+      vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
     end,
   },
 }
