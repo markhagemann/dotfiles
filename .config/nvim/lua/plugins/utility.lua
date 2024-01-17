9e4ce4346b87120b0b01664f8832c02ac30dc474
Mark Hagemann <mhagemann@trevipay.com>
2024-01-17 12:32:10 +1100

    Update autosave settings

--- .config/nvim/lua/plugins/utility.lua
+++ .config/nvim/lua/plugins/utility.lua
@@ -4,6 +4,7 @@ return {
     event = { "BufLeave" },
     config = function()
       require('autosave').setup {
+        events = {'BufLeave'},
         -- your configuration comes here
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
