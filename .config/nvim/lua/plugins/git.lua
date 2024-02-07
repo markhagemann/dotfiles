d86458fcd633279ab40555e24ec6bbf61aab81d3
Mark Hagemann <mhagemann@trevipay.com>
2024-02-07 15:56:57 +1100

    Update gitsigns and utility

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -2,7 +2,18 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     event = "BufEnter",
-    opts = {},
+    config = function()
+      require("git-worktree").setup({
+        current_line_blame = true,
+        current_line_blame_opts = {
+          virt_text = true,
+          virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+          delay = 1000,
+          ignore_whitespace = false,
+          virt_text_priority = 100,
+        },
+      })
+    end,
   },
   {
     "ThePrimeagen/git-worktree.nvim",
