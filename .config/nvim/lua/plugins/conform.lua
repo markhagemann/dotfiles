c4b7bb8307190a23dcc6408a6b5427134e6bfa2b
Mark Hagemann <mhagemann@trevipay.com>
2024-03-25 16:26:14 +1100

    Add conform to take care of formatting

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,69 @@
+local js_formatters = {
+  "eslint_d" --[[ , { "prettierd", "prettier" } ]],
+}
+
+return {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {
+    -- {
+    --   "<leader>lf",
+    --   ":Format<CR>",
+    --   mode = "v",
+    --   desc = "[L]SP [F]ormat Range",
+    -- },
+    {
+      "<leader>ldf",
+      ":FormatDisable<CR>",
+      mode = "n",
+      desc = "[L]SP [D]isable Formatting",
+    },
+    {
+      "<leader>lef",
+      ":FormatEnable<CR>",
+      mode = "n",
+      desc = "[L]SP [E]nable Formatting",
+    },
+  },
+  init = function()
+    vim.api.nvim_create_user_command("FormatDisable", function(args)
+      vim.g.disable_autoformat = true
+    end, {
+      desc = "Disable autoformat-on-save",
+    })
+
+    vim.api.nvim_create_user_command("FormatEnable", function()
+      vim.g.disable_autoformat = false
+    end, {
+      desc = "Re-enable autoformat-on-save",
+    })
+
+    require("conform").setup({
+      format_on_save = function(bufnr)
+        -- Disable autoformat on certain filetypes
+        local ignore_filetypes = { "sql" }
+        if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+          return
+        end
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        -- Disable autoformat for files in a certain path
+        local bufname = vim.api.nvim_buf_get_name(bufnr)
+        if bufname:match("/node_modules/") then
+          return
+        end
+        return { async = true, lsp_fallback = true }
+      end,
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "isort", "black" },
+        javascript = js_formatters,
+        typescript = js_formatters,
+        vue = js_formatters,
+      },
+    })
+  end,
+}
--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,20 +1,16 @@
 return {
   "nvimtools/none-ls.nvim",
+  event = "LspAttach",
   dependencies = {
     "nvimtools/none-ls-extras.nvim",
-  },
-  keys = {
-    { "<leader>lf", vim.lsp.buf.format, {}, desc = "[L]SP [F]ormat Buffer" },
   },
   config = function()
     local null_ls = require("null-ls")
     null_ls.setup({
       debug = true,
       sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        require("none-ls.diagnostics.eslint"),
-        require("none-ls.code_actions.eslint"),
+        require("none-ls.diagnostics.eslint_d"),
+        require("none-ls.code_actions.eslint_d"),
       },
     })
   end,
