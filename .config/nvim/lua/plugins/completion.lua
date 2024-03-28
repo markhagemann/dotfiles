c37f6f036dd8ec41b2e69d1f39a99fc58140bfa8
Mark Hagemann <mhagemann@trevipay.com>
2024-03-28 12:10:52 +1100

    Remove external stylesheets from nvim-html-css sources

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -264,8 +264,8 @@ return {
               file_extensions = { "css", "sass", "scss" }, -- set the local filetypes from which you want to derive classes
               style_sheets = {
                 -- example of remote styles, only css no js for now
-                "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css",
-                "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css",
+                -- "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css",
+                -- "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css",
               }, -- your configuration here
             },
           },
