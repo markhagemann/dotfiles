a3430b366e1cc23e46e075ad8571d5f7b10bbe8d
Mark Hagemann <mhagemann@trevipay.com>
2024-04-15 11:17:16 +1000

    Update telescope settings

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -61,8 +61,9 @@ return {
         pickers = {
           find_files = {
             hidden = true,
-            -- previewer = false,
             theme = "dropdown",
+            previewer = false,
+            follow = true,
           },
           live_grep = {
             additional_args = function(opts)
