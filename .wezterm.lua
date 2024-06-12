3c359e5d215f39789859cc552453a60ef9f85c8f
Mark Hagemann <mhagemann@trevipay.com>
2024-06-13 09:53:57 +1000

    Add wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require("wezterm")
+
+return {
+	color_scheme = "Catppuccin Mocha",
+	enable_tab_bar = false,
+	font_size = 19.0,
+	initial_cols = 130,
+	initial_rows = 33,
+	window_background_opacity = 0.97,
+	window_close_confirmation = "NeverPrompt",
+	window_decorations = "RESIZE",
+}
