9f8a9a4d6afe60f51330f4c0775c76988338df55
Mark Hagemann <mhagemann@trevipay.com>
2024-07-02 13:13:31 +1000

    Add wezterm updates

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,7 +6,14 @@ return {
 	font_size = 19.0,
 	initial_cols = 130,
 	initial_rows = 33,
+	max_fps = 240,
 	window_background_opacity = 0.97,
 	window_close_confirmation = "NeverPrompt",
 	window_decorations = "RESIZE",
+	window_padding = {
+		left = 15,
+		right = 15,
+		top = 10,
+		bottom = 0,
+	},
 }
