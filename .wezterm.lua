899b22e55a1dc64dec0f629d66b598f0fd21bf8b
Mark Hagemann <mhagemann@trevipay.com>
2024-07-17 21:28:09 +1000

    Update wezterm font settings

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,12 +1,15 @@
 local wezterm = require("wezterm")
 
 return {
-	color_scheme = "Catppuccin Mocha",
+	-- color_scheme = "Catppuccin Mocha",
+	color_scheme = "tokyonight_moon",
 	enable_tab_bar = false,
 	font = wezterm.font_with_fallback({
+		{ family = "SpaceMono Nerd Font", harfbuzz_features = { "calt=0", "clig=0", "liga=0" } },
+		{ family = "ZedMono Nerd Font", weight = "Medium" },
 		{ family = "JetBrainsMono Nerd Font", weight = "Medium" },
 	}),
-	font_size = 18.5,
+	font_size = 18,
 	initial_cols = 130,
 	initial_rows = 33,
 	max_fps = 240,
