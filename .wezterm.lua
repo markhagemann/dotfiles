5beb3060384fab83eecd41b4857abbc66c7b712b
Mark Hagemann <mhagemann@trevipay.com>
2024-09-19 09:23:26 +1000

    Update font preference for wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,11 +8,12 @@ return {
 	enable_tab_bar = false,
 	enable_wayland = false,
 	font = wezterm.font_with_fallback({
+		{ family = "OverpassM Nerd Font", weight = 600 },
 		{ family = "SpaceMono Nerd Font", harfbuzz_features = { "calt=0" } },
 		{ family = "JetBrainsMono Nerd Font", weight = "Medium" },
 	}),
 	dpi = 96,
-	font_size = 14.5,
+	font_size = 27.5,
 	freetype_load_flags = "NO_HINTING",
 	freetype_render_target = "HorizontalLcd",
 	front_end = "OpenGL",
@@ -22,7 +23,7 @@ return {
 	max_fps = 240,
 	window_background_opacity = 0.95,
 	window_close_confirmation = "NeverPrompt",
-	window_decorations = "NONE",
+	window_decorations = "RESIZE",
 	window_padding = {
 		left = 5,
 		right = 5,
