fa52a211b35610528381f62414646d63554f73dd
Mark Hagemann <mhagemann09@gmail.com>
2024-09-23 14:58:56 +1000

    Update wezterm settings

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,6 +1,6 @@
 local wezterm = require("wezterm")
 
-local is_darwin = function()
+local is_linux = function()
 	return wezterm.target_triple:find("darwin") ~= nil
 end
 
@@ -20,7 +20,7 @@ return {
 	dpi_by_screen = {
 		["Built-in Retina Display"] = 144,
 	},
-	font_size = 20,
+	font_size = is_linux and 15 or 20,
 	freetype_load_flags = "NO_HINTING",
 	freetype_render_target = "HorizontalLcd",
 	front_end = "OpenGL",
@@ -28,9 +28,9 @@ return {
 	initial_rows = 40,
 	macos_window_background_blur = 6,
 	max_fps = 240,
-	window_background_opacity = is_darwin and 0.95 or 0.99,
+	window_background_opacity = is_linux and 0.99 or 0.95,
 	window_close_confirmation = "NeverPrompt",
-	window_decorations = is_darwin and "RESIZE" or "NONE",
+	window_decorations = is_linux and "NONE" or "RESIZE",
 	window_padding = {
 		left = 10,
 		right = 5,
