7de18c94ab5cef84674ea739042b644d7a891810
Mark Hagemann <mhagemann@trevipay.com>
2024-10-17 19:11:21 +1000

    Update cutlass

--- .config/nvim/lua/plugins/utility.lua
+++ .config/nvim/lua/plugins/utility.lua
@@ -208,6 +208,7 @@ return {
     "gbprod/cutlass.nvim",
     lazy = false,
     opts = {
+      cut_key = "x",
       -- your configuration comes here
       -- or don't set opts to use the default settings
       -- refer to the configuration section below
