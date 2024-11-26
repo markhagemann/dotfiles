15cbe1060dd59489e30f866705482bcf93dddabb
Mark Hagemann <mhagemann@trevipay.com>
2024-11-26 23:40:50 +1000

    Try smear-cursor instead of beacon for nvim

--- dot_config/nvim/lua/plugins/utility.lua
+++ dot_config/nvim/lua/plugins/utility.lua
@@ -87,18 +87,18 @@ return {
       },
     },
   },
-  {
-    "danilamihailov/beacon.nvim",
-    event = "BufEnter",
-    opts = {
-      fps = 60, --- integer how smooth the animation going to be
-      speed = 1, --- integer speed at which animation goes
-      width = 5, --- integer width of the beacon window
-      window_events = { "WinEnter", "FocusGained" }, -- table<string> what events trigger cursor highlight
-      winblend = 0, --- integer starting transparency of beacon window :h winblend
-      highlight = { bg = "#5f87cd", ctermbg = 21 }, -- vim.api.keyset.highlight table passed to vim.api.nvim_set_hl
-    },
-  },
+  -- {
+  --   "danilamihailov/beacon.nvim",
+  --   event = "BufEnter",
+  --   opts = {
+  --     fps = 60, --- integer how smooth the animation going to be
+  --     speed = 1, --- integer speed at which animation goes
+  --     width = 5, --- integer width of the beacon window
+  --     window_events = { "WinEnter", "FocusGained" }, -- table<string> what events trigger cursor highlight
+  --     winblend = 0, --- integer starting transparency of beacon window :h winblend
+  --     highlight = { bg = "#5f87cd", ctermbg = 21 }, -- vim.api.keyset.highlight table passed to vim.api.nvim_set_hl
+  --   },
+  -- },
   {
     "dstein64/vim-startuptime",
     cmd = "StartupTime",
@@ -478,6 +478,36 @@ return {
         },
       })
     end,
+  },
+  {
+    "sphamba/smear-cursor.nvim",
+    event = "BufEnter",
+    opts = {
+      -- stiffness = 0.6,
+      -- trailing_stiffness = 0.3,
+      -- trailing_exponent = 0.1,
+      -- distance_stop_animating = 0.1,
+      -- hide_target_hack = false,
+      -- Cursor color. Defaults to Cursor gui color
+      cursor_color = "#5f87cd",
+
+      -- Background color. Defaults to Normal gui background color
+      -- normal_bg = "#282828",
+
+      -- Smear cursor when switching buffers
+      -- smear_between_buffers = true,
+
+      -- Smear cursor when moving within line or to neighbor lines
+      -- smear_between_neighbor_lines = true,
+
+      -- Use floating windows to display smears over wrapped lines or outside buffers.
+      -- May have performance issues with other plugins.
+      use_floating_windows = false,
+
+      -- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
+      -- Smears will blend better on all backgrounds.
+      -- legacy_computing_symbols_support = false,
+    },
   },
   { "sitiom/nvim-numbertoggle", event = "BufEnter" },
   {
