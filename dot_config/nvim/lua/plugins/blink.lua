3a84e48b29cb66dd836a3fd9a30c884bf427e6cc
Mark Hagemann <mhagemann09@gmail.com>
2025-06-29 19:57:15 +1000

    Update blink

--- dot_config/nvim/lua/plugins/blink.lua
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -37,20 +37,20 @@ return {
         scrollbar = true,
         draw = {
           -- columns = { { "kind_icon", "label", "label_description", gap = 1 } },
-          components = {
-            kind_icon = {
-              ellipsis = false,
-              text = function(ctx)
-                local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
-                return kind_icon
-              end,
-              -- Optionally, you may also use the highlights from mini.icons
-              highlight = function(ctx)
-                local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
-                return hl
-              end,
-            },
-          },
+          -- components = {
+          --   kind_icon = {
+          --     ellipsis = false,
+          --     text = function(ctx)
+          --       local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
+          --       return kind_icon
+          --     end,
+          --     -- Optionally, you may also use the highlights from mini.icons
+          --     highlight = function(ctx)
+          --       local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+          --       return hl
+          --     end,
+          --   },
+          -- },
           treesitter = { "lsp" },
         },
       },
@@ -129,7 +129,39 @@ return {
     appearance = {
       use_nvim_cmp_as_default = true,
       nerd_font_variant = "mono",
-      -- kind_icons = {},
+      kind_icons = {
+        Copilot = "",
+        Text = "󰉿",
+        Method = "󰊕",
+        Function = "󰊕",
+        Constructor = "󰒓",
+
+        Field = "󰜢",
+        Variable = "󰆦",
+        Property = "󰖷",
+
+        Class = "󱡠",
+        Interface = "󱡠",
+        Struct = "󱡠",
+        Module = "󰅩",
+
+        Unit = "󰪚",
+        Value = "󰦨",
+        Enum = "󰦨",
+        EnumMember = "󰦨",
+
+        Keyword = "󰻾",
+        Constant = "󰏿",
+
+        Snippet = "󱄽",
+        Color = "󰏘",
+        File = "󰈔",
+        Reference = "󰬲",
+        Folder = "󰉋",
+        Event = "󱐋",
+        Operator = "󰪚",
+        TypeParameter = "󰬛",
+      },
     },
     keymap = {
       ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
