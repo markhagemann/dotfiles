1a078b24b21822494231324b66c31caf7aed214c
Mark Hagemann <mhagemann09@gmail.com>
2025-07-02 00:30:26 +1000

    Update neovim fold settings and options

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,5 +1,6 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+-- Global variables
+Homedir = os.getenv("HOME")
+Sessiondir = vim.fn.stdpath("data") .. "/sessions"
 
 -- Each entry can be:
 -- * the name of a detector function like `lsp` or `cwd`
@@ -13,8 +14,22 @@ vim.g.root_spec = { "lsp", { ".git", "lua" }, "cwd" }
 -- mini.animate will also be disabled.
 vim.g.bigfile_size = 1024 * 1024 * 1.5 -- 1.5 MB
 
+-- Global options
+vim.g.mapleader = " " -- space is the leader!
+vim.g.maplocalleader = "\\"
+vim.g.loaded_perl_provider = 0 -- Do not load Perl
+
 local opt = vim.opt
 
+-- Buffer options
+opt.autoindent = true
+opt.expandtab = true -- Use spaces instead of tabs
+opt.shiftwidth = 2 -- Size of an indent
+opt.smartindent = true -- Insert indents automatically
+opt.softtabstop = 2 -- Number of spaces tabs count for
+opt.tabstop = 2 -- Number of spaces in a tab
+
+-- Vim options
 opt.autowrite = true -- Enable auto write
 -- only set clipboard if not in ssh, to make sure the OSC 52
 -- integration works automatically. Requires Neovim >= 0.10.0
@@ -23,8 +38,8 @@ opt.completeopt = "menu,menuone,noselect"
 opt.conceallevel = 0 -- Don't conceal anything
 opt.confirm = true -- Confirm to save changes before exiting modified buffer
 opt.cursorline = true -- Enable highlighting of the current line
-opt.expandtab = true -- Use spaces instead of tabs
-opt.fillchars = {
+opt.emoji = false -- Disable emoji support
+opt.fillchars = { -- Fill characters for various elements
   foldopen = "",
   foldclose = "",
   fold = " ",
@@ -32,41 +47,50 @@ opt.fillchars = {
   diff = "╱",
   eob = " ",
 }
-opt.foldlevel = 99
+opt.foldenable = true -- Enable folding
+opt.foldcolumn = "1" -- Fold column width
+opt.foldlevel = 99 -- Using ufo.nvim, so set a high default fold level
+opt.foldlevelstart = 99 -- Show all folds when opening a file
 opt.formatoptions = "jcroqlnt" -- tcqj
-opt.grepformat = "%f:%l:%c:%m"
-opt.grepprg = "rg --vimgrep"
+opt.grepformat = "%f:%l:%c:%m" -- Format for grep results
+opt.grepprg = "rg --vimgrep" -- Use ripgrep for grep
 opt.ignorecase = true -- Ignore case
 opt.inccommand = "nosplit" -- preview incremental substitute
-opt.jumpoptions = "view"
+opt.jumpoptions = "view" -- Keep cursor position when jumping
 opt.laststatus = 3 -- global statusline
 opt.linebreak = true -- Wrap lines at convenient points
 opt.list = true -- Show some invisible characters (tabs...
 opt.mouse = "a" -- Enable mouse mode
 opt.number = true -- Print line number
+opt.numberwidth = 2 -- Set number column width
 opt.pumblend = 0 -- Popup blend
 opt.pumheight = 10 -- Maximum number of entries in a popup
 opt.relativenumber = true -- Relative line numbers
 opt.scrolloff = 4 -- Lines of context
 opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp", "folds" }
 opt.shiftround = true -- Round indent
-opt.shiftwidth = 2 -- Size of an indent
-opt.shortmess:append({ W = true, I = true, c = true, C = true })
+opt.shortmess = {
+  A = true, -- ignore annoying swap file messages
+  c = true, -- Do not show completion messages in command line
+  F = true, -- Do not show file info when editing a file, in the command line
+  I = true, -- Do not show the intro message
+  W = true, -- Do not show "written" in command line when writing
+}
 opt.showmode = false -- Dont show mode since we have a statusline
+opt.showmatch = true -- Show matching brackets by flickering
 opt.sidescrolloff = 8 -- Columns of context
 opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
 opt.smartcase = true -- Don't ignore case with capitals
-opt.smartindent = true -- Insert indents automatically
-opt.spelllang = { "en" }
+opt.smoothscroll = true -- Smooth scrolling
+opt.spelllang = { "en" } -- Set spell checking language
 opt.spelloptions:append("noplainbuffer")
 opt.splitbelow = true -- Put new windows below current
-opt.splitkeep = "screen"
+opt.splitkeep = "screen" -- Keep the screen position when splitting
 opt.splitright = true -- Put new windows right of current
-opt.tabstop = 2 -- Number of spaces tabs count for
 opt.termguicolors = true -- True color support
 opt.timeoutlen = vim.g.vscode and 1000 or 300 -- Lower than default (1000) to quickly trigger mini.clue
-opt.undofile = true
-opt.undolevels = 10000
+opt.undofile = true -- Enable persistent undo
+opt.undolevels = 10000 -- Number of undos to remember
 opt.updatetime = 200 -- Save swap file and trigger CursorHold
 opt.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
 opt.wildmode = "longest:full,full" -- Command-line completion mode
@@ -75,3 +99,15 @@ opt.wrap = false -- Disable line wrap
 
 -- Fix markdown indentation settings
 vim.g.markdown_recommended_style = 0
+
+-- Create folders for our backups, undos, swaps and sessions if they don't exist
+vim.schedule(function()
+  vim.cmd("silent call mkdir(stdpath('data').'/backups', 'p', '0700')")
+  vim.cmd("silent call mkdir(stdpath('data').'/undos', 'p', '0700')")
+  vim.cmd("silent call mkdir(stdpath('data').'/swaps', 'p', '0700')")
+  vim.cmd("silent call mkdir(stdpath('data').'/sessions', 'p', '0700')")
+
+  opt.backupdir = vim.fn.stdpath("data") .. "/backups" -- Use backup files
+  opt.directory = vim.fn.stdpath("data") .. "/swaps" -- Use Swap files
+  opt.undodir = vim.fn.stdpath("data") .. "/undos" -- Set the undo directory
+end)
--- dot_config/nvim/lua/plugins/nvim-ufo.lua
+++ dot_config/nvim/lua/plugins/nvim-ufo.lua
@@ -5,16 +5,15 @@ local handler = function(virtText, lnum, endLnum, width, truncate)
   local targetWidth = width - sufWidth
   local curWidth = 0
   for _, chunk in ipairs(virtText) do
-    local chunkText = chunk1
+    local chunkText = chunk[1]
     local chunkWidth = vim.fn.strdisplaywidth(chunkText)
     if targetWidth > curWidth + chunkWidth then
       table.insert(newVirtText, chunk)
     else
       chunkText = truncate(chunkText, targetWidth - curWidth)
-      local hlGroup = chunk2
+      local hlGroup = chunk[2]
       table.insert(newVirtText, { chunkText, hlGroup })
       chunkWidth = vim.fn.strdisplaywidth(chunkText)
-      -- str width returned from truncate() may less than 2nd argument, need padding
       if curWidth + chunkWidth < targetWidth then
         suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
       end
@@ -52,35 +51,6 @@ return {
       require("ufo").setup({
         -- close_fold_kinds = { "imports" },
         fold_virt_text_handler = handler,
-        provider_selector = function(_, filetype, buftype)
-          -- use nested markdown folding
-          if filetype == "markdown" then
-            return ""
-          end
-
-          -- return ftMap[filetype] or { "treesitter", "indent" }
-          -- return { "treesitter", "indent" }
-          local function handleFallbackException(bufnr, err, providerName)
-            if type(err) == "string" and err:match("UfoFallbackException") then
-              return require("ufo").getFolds(bufnr, providerName)
-            else
-              return require("promise").reject(err)
-            end
-          end
-
-          -- only use indent until a file is opened
-          return (filetype == "" or buftype == "nofile") and "indent"
-            or function(bufnr)
-              return require("ufo")
-                .getFolds(bufnr, "lsp")
-                :catch(function(err)
-                  return handleFallbackException(bufnr, err, "treesitter")
-                end)
-                :catch(function(err)
-                  return handleFallbackException(bufnr, err, "indent")
-                end)
-            end
-        end,
       })
       vim.keymap.set("n", "zO", function()
         require("ufo").openAllFolds()
