c832c20f8e63acf855b42da96209d6b1582f5635
Mark Hagemann <mhagemann09@gmail.com>
2025-07-04 00:12:42 +1000

    Use which-key instead of mini.clue

--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -8,7 +8,6 @@ return {
     version = "*",
     config = function()
       require("mini.ai").setup()
-      local clue = require("mini.clue")
       local diff = require("mini.diff")
       diff.setup({
         view = {
@@ -50,50 +49,6 @@ return {
         },
       })
       require("mini.surround").setup()
-
-      clue.setup({
-        clues = {
-          -- Enhance this by adding descriptions for <Leader> mapping groups
-          clue.gen_clues.builtin_completion(),
-          clue.gen_clues.g(),
-          clue.gen_clues.marks(),
-          clue.gen_clues.registers(),
-          clue.gen_clues.windows(),
-          clue.gen_clues.z(),
-        },
-        triggers = {
-          -- Leader triggers
-          { mode = "n", keys = "<leader>" },
-          { mode = "x", keys = "<leader>" },
-
-          -- Built-in completion
-          { mode = "i", keys = "<C-x>" },
-
-          -- `g` key
-          { mode = "n", keys = "g" },
-          { mode = "x", keys = "g" },
-
-          -- Marks
-          { mode = "n", keys = "'" },
-          { mode = "x", keys = "'" },
-
-          -- Registers
-          { mode = "n", keys = '"' },
-          { mode = "x", keys = '"' },
-          { mode = "i", keys = "<C-r>" },
-          { mode = "c", keys = "<C-r>" },
-
-          -- Window commands
-          { mode = "n", keys = "<C-w>" },
-
-          -- `z` key
-          { mode = "n", keys = "z" },
-          { mode = "x", keys = "z" },
-        },
-        window = {
-          delay = 100,
-        },
-      })
 
       files.setup({
         mappings = {
