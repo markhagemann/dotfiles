d1745349fe573616eac7a8972486d7737a5baff7
Mark Hagemann <mhagemann09@gmail.com>
2025-08-02 16:18:31 +1000

    Update lsp issues and remove some unused plugins

--- dot_config/nvim/lua/plugins/ui/ui.lua
+++ dot_config/nvim/lua/plugins/ui/ui.lua
@@ -39,54 +39,6 @@ return {
     end,
   },
   {
-    "jake-stewart/auto-cmdheight.nvim",
-    event = "VeryLazy",
-    opts = {
-      -- max cmdheight before displaying hit enter prompt.
-      max_lines = 5,
-
-      -- number of seconds until the cmdheight can restore.
-      duration = 2,
-
-      -- whether key press is required to restore cmdheight.
-      remove_on_key = true,
-    },
-  },
-  {
-    "jinh0/eyeliner.nvim",
-    event = "VeryLazy",
-    config = function()
-      vim.api.nvim_set_hl(0, "EyelinerPrimary", { fg = "#569CD6", bold = true, underline = true })
-      vim.api.nvim_set_hl(0, "EyelinerSecondary", { fg = "#C586C0", underline = true })
-
-      require("eyeliner").setup({
-        -- show highlights only after keypress
-        highlight_on_key = true,
-
-        -- dim all other characters if set to true (recommended!)
-        dim = false,
-
-        -- set the maximum number of characters eyeliner.nvim will check from
-        -- your current cursor position; this is useful if you are dealing with
-        -- large files: see https://github.com/jinh0/eyeliner.nvim/issues/41
-        max_length = 9999,
-
-        -- filetypes for which eyeliner should be disabled;
-        -- e.g., to disable on help files:
-        -- disabled_filetypes = {"help"}
-        disabled_filetypes = {},
-
-        -- buftypes for which eyeliner should be disabled
-        -- e.g., disabled_buftypes = {"nofile"}
-        disabled_buftypes = {},
-
-        -- add eyeliner to f/F/t/T keymaps;
-        -- see section on advanced configuration for more information
-        default_keymaps = true,
-      })
-    end,
-  },
-  {
     "rachartier/tiny-glimmer.nvim",
     event = "VeryLazy",
     -- opts = {
--- dot_config/nvim/lua/plugins/utility.lua
+++ dot_config/nvim/lua/plugins/utility.lua
@@ -125,6 +125,13 @@ return {
     opts = {},
   },
   {
+    "folke/persistence.nvim",
+    event = "BufReadPre", -- this will only start session saving when an actual file was opened
+    opts = {
+      -- add any custom options here
+    },
+  },
+  {
     "kburdett/vim-nuuid",
     keys = {
       { "<leader>uu", "<Plug>Nuuid", desc = "Generate uuid" },
@@ -241,22 +248,6 @@ return {
         desc = "Toggle window fullscreen",
       },
     },
-  },
-  {
-    "rmagatti/auto-session", -- auto save session
-    event = "VeryLazy",
-    config = function()
-      require("auto-session").setup({
-        log_level = "error",
-        auto_session_suppress_dirs = {
-          "~/",
-          "~/Downloads",
-          "~/Documents",
-        },
-        auto_session_use_git_branch = true,
-        auto_save_enabled = true,
-      })
-    end,
   },
   {
     "r0nsha/multinput.nvim",
