18a5420ba95150780d89ca94f4f2805349bf7e62
Mark Hagemann <mhagemann09@gmail.com>
2025-08-17 18:27:38 +1000

    Add lazyvim config for render-markdown

--- dot_config/nvim/lua/plugins/markdown.lua
+++ dot_config/nvim/lua/plugins/markdown.lua
@@ -10,9 +10,38 @@ return {
   },
   {
     "MeanderingProgrammer/render-markdown.nvim",
-    ft = { "markdown", "codecompanion" },
-    opts = {},
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    opts = {
+      code = {
+        sign = false,
+        width = "block",
+        right_pad = 1,
+      },
+      heading = {
+        sign = false,
+        icons = {},
+      },
+      checkbox = {
+        enabled = false,
+      },
+    },
+    ft = { "markdown", "norg", "rmd", "org", "codecompanion" },
+    config = function(_, opts)
+      require("render-markdown").setup(opts)
+      Snacks.toggle({
+        name = "Render Markdown",
+        get = function()
+          return require("render-markdown.state").enabled
+        end,
+        set = function(enabled)
+          local m = require("render-markdown")
+          if enabled then
+            m.enable()
+          else
+            m.disable()
+          end
+        end,
+      }):map("<leader>um")
+    end,
   },
   {
     "epwalsh/obsidian.nvim",
@@ -23,7 +52,6 @@ return {
     dependencies = {
       -- Required.
       "nvim-lua/plenary.nvim",
-
       -- see below for full list of optional dependencies 👇
     },
     keys = {
